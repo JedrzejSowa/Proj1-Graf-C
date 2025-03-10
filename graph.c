c6d5b2b0df26838eaf6c9e5b4a4adc751c7fb965
JedrzejSowa <01197856@pw.edu.pl>
2025-03-11 00:52:22 +0100

    directions

--- graph.c
+++ graph.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Maksymalna liczba rogów
+#define MAX_CORNERS 10
+
 void generateGraph(int corners) {
-    if (corners > 10) {
-        printf("This format only supports up to 10 corners.\n");
+    if (corners > MAX_CORNERS) {
+        printf("This format only supports up to %d corners.\n", MAX_CORNERS);
         return;
     }
 
@@ -18,7 +21,7 @@ void generateGraph(int corners) {
     fprintf(file, "Generated Graph:\n\n");
 
     // Inicjalizacja siatki z -1 (puste miejsca)
-    int grid[10][10];
+    int grid[MAX_CORNERS][MAX_CORNERS];
     for (int i = 0; i < corners; i++) {
         for (int j = 0; j < corners; j++) {
             grid[i][j] = -1;
@@ -50,6 +53,66 @@ void generateGraph(int corners) {
         }
     }
 
+    // Tablica do sledzenia wygenerowanych polaczen
+    int connections[MAX_CORNERS][MAX_CORNERS] = {0};
+
+    // Dodanie losowych strzalek miedzy niepustymi rogami
+    for (int i = 0; i < corners; i++) {
+        for (int j = 0; j < corners; j++) {
+            if (grid[i][j] != -1) {
+                // Sprawdz niepuste sasiady i losowo dodaj strzalke
+                if (i > 0 && grid[i - 1][j] != -1 && !connections[grid[i][j]][grid[i - 1][j]]) {  // Nad
+                    int direction = rand() % 3;
+                    if (direction == 0) {
+                        fprintf(file, "[%d] --> [%d]\n", grid[i][j], grid[i - 1][j]);
+                    } else if (direction == 1) {
+                        fprintf(file, "[%d] <-- [%d]\n", grid[i][j], grid[i - 1][j]);
+                    } else {
+                        fprintf(file, "[%d] <-> [%d]\n", grid[i][j], grid[i - 1][j]);
+                    }
+                    connections[grid[i][j]][grid[i - 1][j]] = 1;
+                    connections[grid[i - 1][j]][grid[i][j]] = 1;
+                }
+                if (i < corners - 1 && grid[i + 1][j] != -1 && !connections[grid[i][j]][grid[i + 1][j]]) {  // Pod
+                    int direction = rand() % 3;
+                    if (direction == 0) {
+                        fprintf(file, "[%d] --> [%d]\n", grid[i][j], grid[i + 1][j]);
+                    } else if (direction == 1) {
+                        fprintf(file, "[%d] <-- [%d]\n", grid[i][j], grid[i + 1][j]);
+                    } else {
+                        fprintf(file, "[%d] <-> [%d]\n", grid[i][j], grid[i + 1][j]);
+                    }
+                    connections[grid[i][j]][grid[i + 1][j]] = 1;
+                    connections[grid[i + 1][j]][grid[i][j]] = 1;
+                }
+                if (j > 0 && grid[i][j - 1] != -1 && !connections[grid[i][j]][grid[i][j - 1]]) {  // Lewo
+                    int direction = rand() % 3;
+                    if (direction == 0) {
+                        fprintf(file, "[%d] --> [%d]\n", grid[i][j], grid[i][j - 1]);
+                    } else if (direction == 1) {
+                        fprintf(file, "[%d] <-- [%d]\n", grid[i][j], grid[i][j - 1]);
+                    } else {
+                        fprintf(file, "[%d] <-> [%d]\n", grid[i][j], grid[i][j - 1]);
+                    }
+                    connections[grid[i][j]][grid[i][j - 1]] = 1;
+                    connections[grid[i][j - 1]][grid[i][j]] = 1;
+                }
+                if (j < corners - 1 && grid[i][j + 1] != -1 && !connections[grid[i][j]][grid[i][j + 1]]) {  // Prawo
+                    int direction = rand() % 3;
+                    if (direction == 0) {
+                        fprintf(file, "[%d] --> [%d]\n", grid[i][j], grid[i][j + 1]);
+                    } else if (direction == 1) {
+                        fprintf(file, "[%d] <-- [%d]\n", grid[i][j], grid[i][j + 1]);
+                    } else {
+                        fprintf(file, "[%d] <-> [%d]\n", grid[i][j], grid[i][j + 1]);
+                    }
+                    connections[grid[i][j]][grid[i][j + 1]] = 1;
+                    connections[grid[i][j + 1]][grid[i][j]] = 1;
+                }
+            }
+        }
+    }
+
     // Wypisanie siatki do pliku
     for (int i = 0; i < corners; i++) {
         for (int j = 0; j < corners; j++) {
@@ -71,8 +134,8 @@ int main() {
     printf("Enter the number of corners (max 10): ");
     scanf("%d", &corners);
 
-    if (corners <= 0 || corners > 10) {
-        printf("Number of corners must be between 1 and 10!\n");
+    if (corners <= 0 || corners > MAX_CORNERS) {
+        printf("Number of corners must be between 1 and %d!\n", MAX_CORNERS);
         return 1;
     }
 
